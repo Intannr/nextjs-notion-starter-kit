9d256ffa61f95ca9081de301fadf0e35425cf025
Intannr <126791919+Intannr@users.noreply.github.com>
2023-04-14 10:52:39 +0700

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '8dadf2ab74a24c1082c1d9025c0a04f8',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
